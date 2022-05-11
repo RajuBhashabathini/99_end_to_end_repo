da3d7a580c32e4fc4fa1477fcad68500b596a5d9
[Raju <rajubhashabathini007@gmail.com>
2022-05-11 12:59:40 +0530

    done

--- api_learnlytica/db/db.js
+++ api_learnlytica/db/db.js
@@ -19,7 +19,7 @@ let mysqlConnect = function () {
        
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD, //should be yours 
+        password: process.env.DB_PASSWORD, 
         database: process.env.DB_NAME,
         port: process.env.DB_PORT,
         insecureAuth: true
